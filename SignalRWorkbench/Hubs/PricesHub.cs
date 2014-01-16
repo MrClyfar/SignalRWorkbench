803731a69e3bab431217a73bc81193a4b9e9c591
MrClyfar <myfanwy3>
2014-01-16 21:29:34 +0000

    Initial checkin.

---
+++ SignalRWorkbench/Hubs/PricesHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SignalRWorkbench.Hubs
+{
+    [HubName("pricesHub")]
+    public class PricesHub : Hub
+    {
+        public void GetPrices()
+        {
+            var task = GetPricesAsync();
+
+            
+            task.ContinueWith(t => Clients.Caller.onPricesRecieved(t.Result.Timestamp));
+        }
+
+        private async Task<GetPricesResult> GetPricesAsync()
+        {
+            await Task.Run(() => Thread.Sleep(5000));
+
+            return new GetPricesResult { Timestamp = DateTime.UtcNow };
+        }
+    }
+
+    public class GetPricesResult
+    {
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ SignalRWorkbench/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="modernizr-2.7.1.js" />
+/// <reference path="jquery-2.0.3.js" />
+/// <autosync enabled="true" />
+/// <reference path="bootstrap.js" />
+/// <reference path="respond.js" />
+/// <reference path="jquery.signalr-2.0.1.js" />
